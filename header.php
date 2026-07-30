d8d7f3da21a626216311a5b22dc8eaed9a37e0d3
yethminidesilva378-lab <yethminidesilva378@gmail.com>
2026-07-30 14:14:07 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Online Visitor Management System</title>
+
+    <link rel="stylesheet" href="/vms/css/style.css">
+
+</head>
+
+<body>
+
+<header>
+
+    <h1>Online Visitor Management System</h1>
+
+    <nav>
+
+        <a href="home.php">Home</a> |
+
+        <a href="visitor.php">Visitors</a> |
+
+        <a href="admin.php">Admin</a> |
+
+        <a href="reports.php">Reports</a> |
+
+        <a href="help.php">Help</a> |
+
+        <a href="logout.php">Logout</a>
+
+    </nav>
+
+    <hr>
+
+</header>
